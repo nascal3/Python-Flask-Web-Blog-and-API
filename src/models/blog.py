dc7026396c34795c1028cb3631d0f5cc571514ca
nascal3 <nascal3@gmail.com>
2017-07-04 15:37:41 +0300

    compeleted the API part of the website

--- src/models/blog.py
+++ src/models/blog.py
@@ -7,8 +7,9 @@ from src.models.post import Post
 
 
 class Blog(object):
-    def __init__(self, author, title, description, _id=None):
+    def __init__(self, author, title, description, author_id, _id=None):
         self.author = author
+        self.author_id = author_id
         self.title = title
         self.description = description
         self._id = uuid.uuid4().hex if _id is None else _id
@@ -33,6 +34,7 @@ class Blog(object):
     def json(self):
         return {
             'author':self.author,
+            'author_id':self.author_id,
             'title':self.title,
             'description':self.description,
             '_id':self._id
@@ -40,6 +42,10 @@ class Blog(object):
 
     @classmethod
     def from_mongo(cls, id):
-       blog_data = Database.find_one(collection='blogs', query={'_id':id})
+        blog_data = Database.find_one(collection='blogs', query={'_id':id})
+        return cls(**blog_data)
 
-       return cls(**blog_data)
+    @classmethod
+    def find_by_author(cls, author_id):
+        blog_data = Database.find(collection='blogs', query={'author__id':author_id})
+        return [cls(**blog) for blog in blog_data]
--- src/models/user.py
+++ src/models/user.py
@@ -1,6 +1,10 @@
 import uuid
 
+import datetime
+from flask import session
+
 from src.common.database import Database
+from src.models.blog import Blog
 
 
 class User(object):
@@ -11,7 +15,7 @@ class User(object):
 
     @classmethod
     def get_by_email(cls, email):
-        data = Database.DATABASE.find_one(collection='users', query={'email':email})
+        data = Database.DATABASE.find_one(collection='users', query={'email': email})
         if data is not None:
             return cls(**data)
 
@@ -20,6 +24,7 @@ class User(object):
         data = Database.DATABASE.find_one(collection='users', query={'_id': _id})
         if data is not None:
             return cls(**data)
+
     @staticmethod
     def login_valid(email, password):
         user = User.get_by_email(email=email)
@@ -31,21 +36,41 @@ class User(object):
     def register(cls, email, password):
         user = cls.get_by_email(email=email)
         if user is None:
-           new_user = cls(email, password)
-           new_user.save_to_mongo()
-           return True
+            new_user = cls(email, password)
+            new_user.save_to_mongo()
+            session['email'] = email
+            return True
         else:
-           return False
+            return False
 
+    @staticmethod
+    def login(user_email):
+        session['email'] = user_email
 
-    def login(self):
-        pass
+    @staticmethod
+    def logout():
+        session['email'] = None
 
     def get_blogs(self):
-        pass
+        return Blog.find_by_author(self._id)
+
+    def new_blog(self, title, description):
+        #  author, title, description, author_id
+        blog = Blog(author=self.email, title=title, description=description, author_id=self._id)
+        blog.save_to_mongo()
+
+    @staticmethod
+    def new_post(blog_id, title, content, date=datetime.datetime.utcnow()):
+
+        blog = Blog.from_mongo(blog_id)
+        blog.new_post(title=title, content=content, date=date)
 
     def json(self):
-        pass
+        return {
+            'email': self.email,
+            'password': self.password,
+            '_id': self._id
+        }
 
     def save_to_mongo(self):
-        Database.DATABASE.insert(collection='post', data=self.json())
+        Database.insert(collection='users', data=self.json())
